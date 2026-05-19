22b5567749815f8c2c2d132ab09a5496cee72d73
Adityasingh182 <149592791+Adityasingh182@users.noreply.github.com>
2026-05-19 23:29:29 +0530

    71252

--- orders/models.py
+++ orders/models.py
@@ -13,3 +13,13 @@ class OrderStatus(models.Model):
     
     def __str__(self):
         return self.name
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=true)
+    discount_percentage = models.DecimalsField(max_digits=5, decimal_places=2)
+    is_active = models,BooleanField(default=True)
+    valid_from = models.DataField()
+    valid_until = models.DataField()
+
+    def __str__(self):
+        return self.name
