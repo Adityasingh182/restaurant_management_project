3b58b5ff13dde1d7f913680c49fd14fae880309f
Adityasingh182 <149592791+Adityasingh182@users.noreply.github.com>
2026-05-19 22:59:32 +0530

    71250

--- account/models.py
+++ account/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-
+from .models import OrderStatus
 # Create your models here
 class MenuCategory(models.Model):
     name=models.CharField(max_length=100,unique=True)
 def __str__(self):
     return self.name
+
+class order 
